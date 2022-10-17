5c2118bc3b208322290d3b4c0c953650a47dffc9
Tahseen Humaira <humairatahseen4@gmail.com>
2022-10-17 09:08:30 +0100

    Update primes.py
    Function calculates the first x number of primes.

--- primes.py
+++ primes.py
@@ -3,4 +3,13 @@
 
 def primes(number_of_primes):
     list = []
-    return list
+    for number in range(number_of_primes):
+        if number>1:
+            for i in range (2, number):
+                if (number % i)==0:
+                    break
+            else:
+                list.append(number)
+    print(list)
+
+primes(10)
